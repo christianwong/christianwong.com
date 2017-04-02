934f0e090fa585f6570a12aecce3dbad24da582f
Christian Wong <dev@christianwong.com>
2017-04-02 15:40:57 -0300

    initial version

---
+++ src/main/java/com/christianwong/rest/HelloWorldResource.java
@@ -0,0 +1,24 @@
+package com.christianwong.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+ 
+@Path("/hello")
+public class HelloWorldResource {
+ 
+    @GET
+    @Produces("text/plain")
+    public String getMessage() {
+        return "Hello, World!";
+    }
+
+    @GET
+    @Path("{name}")
+    @Produces("text/plain")
+    public String getMessage(@PathParam("name") String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
